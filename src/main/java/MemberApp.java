aed11da3e33ce246354f7f08ffe736c6e7b5ebd2
ymwoo@hanatour.com <>
2022-04-10 17:12:23 +0900

    관심사 분리
    - AppConfig에서 필요한 구현체를 생성자로 넘기는 것으로 변경하였다.

--- src/main/java/MemberApp.java
+++ src/main/java/MemberApp.java
@@ -2,23 +2,20 @@ import hello.core.config.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
-public class OrderApp {
+public class MemberApp {
     public static void main(String[] args) {
-
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
-
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
-
-        System.out.println("order = " + order.toString());
-        System.out.println("order.calculatePrice = " + order.calculatePrice());
+        Member findMember = memberService.findMember(memberId);
+        System.out.println("findMember = " + findMember);
     }
 }
